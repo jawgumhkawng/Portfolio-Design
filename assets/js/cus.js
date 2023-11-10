58dd019d7427380811a81e71f6a49431500e7a9a
jawgumhkawng <nawbosco007@gmail.com>
2023-11-10 23:03:58 +0630

    upload & commit

---
+++ assets/js/cus.js
@@ -0,0 +1,197 @@
+var waypoint = new Waypoint({
+    element: document.getElementById('about'),
+    handler: function(direction) {
+      if (direction === "down") {
+        document.querySelector( ".navbar" ).classList.add("navbar-control") 
+        document.querySelector( ".navbar" ).classList.add("animate__fadeInDown") 
+       
+        console.log("down")
+      } else {
+        document.querySelector( ".navbar" ).classList.remove("navbar-control")
+        document.querySelector( ".navbar" ).classList.remove("animate__fadeInDown")  
+       
+
+       
+        
+    
+        console.log("up")
+      }
+    },
+    offset: '2%'
+  })
+
+
+  var waypoint = new Waypoint({
+    element: document.getElementById('pricing'),
+    handler: function(direction) {
+      if (direction === "down") {
+        document.querySelector( ".footer-fixed" ).classList.add("show") 
+        document.querySelector( ".footer-fixed" ).classList.add("animate__fadeInDown") 
+        
+        console.log("down")
+      } else {
+        document.querySelector( ".footer-fixed" ).classList.remove("show") 
+        document.querySelector( ".footer-fixed" ).classList.remove("animate__fadeInDown") 
+    
+        console.log("up")
+      }
+    },
+    offset: '5%'
+  })
+
+
+  // theme
+
+   let card = document.querySelectorAll( '.card' );
+
+
+  const changeThemeToDark = () => {
+    document.documentElement.setAttribute('data-theme','dark');
+    document.querySelector('.navbar').classList.remove('shadow-lg');
+    document.querySelector('.navbar').classList.remove('shadow');
+    document.querySelector('.navbar').classList.add('pdf-border-zinc-800');
+    document.querySelector('.navbar').classList.add('border-bottom');
+    
+    localStorage.setItem('data-theme','dark');
+
+    for(let x = 0; x < card.length; x++ ) {
+        card[x].classList.remove('shadow-lg');
+        card[x].classList.remove('border-0');
+        card[x].classList.add('shadow-md');
+        card[x].classList.add('pdf-border-zinc-700');
+        card[x].classList.add('shadow-red-400');
+        
+    }
+  }
+
+
+  const changeThemeToLight = () => {
+    document.documentElement.setAttribute('data-theme','light');
+    document.querySelector('.navbar').classList.add('shadow-lg');
+    document.querySelector('.navbar').classList.remove('border-bottom');
+    document.querySelector('.navbar').classList.add('pdf-border-zinc-800');
+    document.querySelector('.navbar').classList.add("shadow-zinc-200","shadow-lg");
+
+    localStorage.setItem('data-theme','light');
+
+     for(let x = 0; x < card.length; x++ ) {
+        card[x].classList.add('shadow-lg');
+        card[x].classList.add('border-0');
+        card[x].classList.remove('shadow-md');
+        card[x].classList.add('pdf-border-zinc-700');
+        card[x].classList.add('shadow-red-400');
+        
+    }
+  }
+
+  
+  let changeTheme = document.getElementById('changeTheme');
+
+  changeTheme.addEventListener( 'change', () => {
+    let theme = localStorage.getItem('data-theme');
+    if (theme === 'light') {
+      changeThemeToDark();
+     } else {
+      changeThemeToLight()
+    }
+  })
+
+
+  let theme = localStorage.getItem('data-theme');
+
+  if (theme ===  'dark'){
+    changeThemeToDark()
+    changeTheme.setAttribute('checked', 'checked')
+  } else {
+    changeThemeToLight()
+  }
+
+  // menu-icon
+
+  let menuIcon = document.querySelector('.menu-icon')
+  menuIcon.addEventListener('click', () => {
+      menuIcon.classList.toggle('show');
+  })
+
+  // slick
+
+ $('.portfolio').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 900,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+// menu animate
+
+
+// let navbar = document.querySelector('.navbar');
+
+// menu.onclick = () =>{
+ 
+//   navbar.classList.toggle('active');
+// }
+
+
+// let navLinks = document.querySelectorAll('.navbar li a');
+
+// window.onscroll = () =>{
+
+
+//   navbar.classList.remove('active');
+
+//   section.forEach(sec =>{
+
+//     let top = window.scrollY;
+//     let height = sec.offsetHeight;
+//     let offset = sec.offsetTop - 150;
+//     let id = sec.getAttribute('id');
+
+//     if(top >= offset && top < offset + height){
+//       navLinks.forEach(links =>{
+//         links.classList.remove('active');
+//         document.querySelector('.navbar li a[href*='+id+']').classList.add('active');
+//       });
+//     };
+
+//   });
+
+// }
---
+++ assets/js/scrollReveal.js
@@ -0,0 +1,226 @@
+// scrollreveal 
+
+ScrollReveal({
+    origin: 'left',
+    distance: '300px',
+    duration: 2200,
+    reset: true,
+  }).reveal('.headline1');
+  
+  ScrollReveal({
+    origin: 'bottom',
+    distance: '300px',
+    duration: 2280, 
+    reset: true,
+  }).reveal('.headline2',{
+    scale: 0.6,
+  });
+  
+  ScrollReveal({
+    origin: 'left',
+    distance: '300px',
+    duration: 2380, 
+    reset: true,
+  }).reveal('.service1',{
+    scale: 0.7,
+  });
+  
+  ScrollReveal({
+    origin: 'bottom',
+    distance: '300px',
+    duration: 2380, 
+    reset: true,
+  }).reveal('.service2',{
+    scale: 0.6,
+  });
+  
+  ScrollReveal({
+    origin: 'bottom',
+    distance: '230px',
+    duration: 2300, 
+    reset: true,
+  }).reveal('.serviceIcon',{
+    scale: 0.6,
+  });
+  
+  ScrollReveal({
+    origin: 'bottom',
+    distance: '300px',
+    duration: 2800, 
+    reset: true,
+  }).reveal('.service3',{
+    scale: 0.6,
+  });
+  
+  ScrollReveal({
+    origin: 'left',
+    distance: '230px',
+    duration: 2200, 
+    reset: true,
+  }).reveal('.about1',{
+    scale: 0.6,
+  });
+  
+  ScrollReveal({
+    origin: 'bottom',
+    distance: '230px',
+    duration: 2300, 
+    reset: true,
+  }).reveal('.about2');
+  
+  ScrollReveal({
+    origin: 'left',
+    distance: '300px',
+    duration: 2300, 
+    reset: true,
+  }).reveal('.aboutIcon');
+  
+  ScrollReveal({
+    origin: 'bottom',
+    distance: '230px',
+    duration: 2600, 
+    reset: true,
+  }).reveal('.aboutIcon2');
+  
+  ScrollReveal({
+    origin: 'left',
+    distance: '230px',
+    duration: 2600, 
+    reset: true,
+  }).reveal('.porfolio1');
+  
+  ScrollReveal({
+    origin: 'bottom',
+    distance: '230px',
+    duration: 2300, 
+    reset: true,
+  }).reveal('.porfolio2');
+  
+  ScrollReveal({
+    origin: 'bottom',
+    distance: '230px',
+    duration: 2990, 
+    reset: true,
+  }).reveal('.porfolio3');
+  
+  ScrollReveal({
+    origin: 'left',
+    distance: '230px',
+    duration: 2390, 
+    reset: true,
+  }).reveal('.teammate1');
+  
+  ScrollReveal({
+    origin: 'bottom',
+    distance: '230px',
+    duration: 2590, 
+    reset: true,
+  }).reveal('.teammate',{
+    interval: 200,
+    scale: 0.7,
+  });
+  
+  ScrollReveal({
+    origin: 'left',
+    distance: '230px',
+    duration: 2700, 
+    reset: true,
+  }).reveal('.teamName',{
+    scale: 0.8,
+  });
+  
+  ScrollReveal({
+    origin: 'bottom',
+    distance: '230px',
+    duration: 2590, 
+    reset: true,
+  }).reveal('.test',{
+    interval: 200,
+    scale: 0.7,
+  });
+  
+  ScrollReveal({
+    origin: 'left',
+    distance: '230px',
+    duration: 2590, 
+    reset: true,
+  }).reveal('.testName',{
+    scale: 0.8,
+  });
+  
+  ScrollReveal({
+    origin: 'bottom',
+    distance: '230px',
+    duration: 2590, 
+    reset: true,
+  }).reveal('.price',{
+    interval: 200,
+    scale: 0.7,
+  });
+  
+  ScrollReveal({
+    origin: 'left',
+    distance: '230px',
+    duration: 2590, 
+    reset: true,
+  }).reveal('.priceName',{
+    scale: 0.8,
+  });
+  
+  ScrollReveal({
+    origin: 'left',
+    distance: '230px',
+    duration: 2590, 
+    reset: true,
+  }).reveal('.contactName',{
+    scale: 0.8,
+  });
+  
+  ScrollReveal({
+    origin: 'bottom',
+    distance: '230px',
+    duration: 2590, 
+    reset: true,
+  }).reveal('.locationName',{
+    interval: 200,
+    scale: 0.8,
+  });
+  
+  ScrollReveal({
+    origin: 'bottom',
+    distance: '230px',
+    duration: 2590, 
+    reset: true,
+  }).reveal('.map',{
+    scale: 0.8,
+  });
+  
+  ScrollReveal({
+    origin: 'bottom',
+    distance: '230px',
+    duration: 2590, 
+    reset: true,
+  }).reveal('.formName',{
+    interval: 200,
+    scale: 0.8,
+  });
+  
+  ScrollReveal({
+    origin: 'left',
+    distance: '230px',
+    duration: 2700, 
+    reset: true,
+  }).reveal('.formBTN',{
+    scale: 0.8,
+  });
+  
+  ScrollReveal({
+    origin: 'bottom',
+    distance: '230px',
+    duration: 2100, 
+    reset: true,
+  }).reveal('.footerName',{
+    interval: 140,
+    scale: 0.8,
+    viewFactor: 1.0,
+  });
